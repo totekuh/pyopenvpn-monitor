62573c24c63c488529b5dd9ca5ea77d9ee866097
retr0 <you@example.com>
2020-05-03 00:48:11 -0400

    add docker file

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TOKEN = "GET_YOUR_OWN_TOKEN_FROM_@BOTFATHER"
 WHITELIST = ['your_mom']
-OPENVPN_STATUS_LOG_FILE = "/etc/openvpn/openvpn-status.log"
+OPENVPN_STATUS_LOG_FILE = "/ovpn-bot/openvpn-status.log"
